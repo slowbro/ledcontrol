8493967661148a17c260c7280bbe23a727e7fd8c
Katelyn Schiesser <katelyn.schiesser@gmail.com>
2018-04-18 10:50:56 -0700

    sorta-genericize build script

--- src/led_controller/build.sh
+++ src/led_controller/build.sh
@@ -5,13 +5,13 @@ WD=$PWD/${0%/*}
 
 OUT=`/usr/share/arduino/arduino-builder \
     -compile -logger=machine -hardware /usr/share/arduino/hardware \
-    -hardware /home/katelyn/.arduino15/packages -tools /usr/share/arduino/tools-builder \
-    -tools /home/katelyn/.arduino15/packages -libraries /home/katelyn/Arduino/libraries \
+    -hardware $HOME/.arduino15/packages -tools /usr/share/arduino/tools-builder \
+    -tools $HOME/.arduino15/packages -libraries $HOME/Arduino/libraries \
     -fqbn=arduino:avr:uno:uploadspeed=19200 -ide-version=10805 -build-path $WD/tmp/build \
     -warnings=all -build-cache $WD/tmp/cache -prefs=build.warn_data_percentage=75 \
-    -prefs=runtime.tools.arduinoOTA.path=/home/katelyn/.arduino15/packages/arduino/tools/arduinoOTA/1.1.1 \
-    -prefs=runtime.tools.avrdude.path=/home/katelyn/.arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9 \
-    -prefs=runtime.tools.avr-gcc.path=/home/katelyn/.arduino15/packages/arduino/tools/avr-gcc/4.9.2-atmel3.5.4-arduino2 \
+    -prefs=runtime.tools.arduinoOTA.path=$HOME/.arduino15/packages/arduino/tools/arduinoOTA/1.1.1 \
+    -prefs=runtime.tools.avrdude.path=$HOME/.arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9 \
+    -prefs=runtime.tools.avr-gcc.path=$HOME/.arduino15/packages/arduino/tools/avr-gcc/4.9.2-atmel3.5.4-arduino2 \
     -verbose $WD/led_controller.ino`
 RET=$?
 echo "$OUT" | egrep -v '^===info'
